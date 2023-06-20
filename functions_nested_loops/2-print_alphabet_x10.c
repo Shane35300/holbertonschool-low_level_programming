84158b706e5c3bc022cf179aeafa60edd01f7e70
Shane35300 <6874@holbertonstudents.com>
2023-06-20 12:43:50 -0700

    oki

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - main function
+ * Description: print_alphabet_x10 - print the alphabet twice
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 2)
+	{
+		char lettre = 'a';
+
+		while (lettre <= 'z')
+		{
+			_putchar(lettre);
+			lettre++;
+		}
+		_putchar('\n');
+	}
+}
+
