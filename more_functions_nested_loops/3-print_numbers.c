bb802282ede4dc3b0c3b4da31e6fba59cf42c05d
Shane35300 <6874@holbertonstudents.com>
2023-06-23 07:30:00 -0700

    pl

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -8,7 +8,7 @@ void print_numbers(void)
 {
 	int number;
 
-	for (number = 0; 0 <= number && number <= 9 ; number++)
+	for (number = 0; number >= 0 && number <= 9 ; number++)
 	{
 		_putchar('0' + number);
 	}
