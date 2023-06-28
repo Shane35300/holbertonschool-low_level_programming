b046ae06d17b20bd2ab5927d1a5589d129ee0f9e
shane <6874@holbertonstudents.com>
2023-06-28 10:08:03 +0200

    goodoogoood

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * rev_string - print
+ * puts_half - print
  * Description : print reversed
  *@str: a pointer/
  */
@@ -8,24 +8,24 @@ void puts_half(char *str)
 {
 	int nombre = 0;
 	int nb = 0;
-	char temp;
 
-	 while (str[nombre] != '\0')
-	 {
+	while (str[nombre] != '\0')
+	{
 		nombre++;
-	 }
-	 if (nombre % 2 != 0)
-	 {
+	}
+	if (nombre % 2 != 0)
+	{
 		for (nb = (nombre - 1) / 2 ; nb <= nombre ; nb++)
 		{
 			_putchar(str[nb]);
-		}
-	 }
-	 else
-	 {
+			}
+	}
+	else
+	{
 		for (nb = nombre / 2 ; nb <= nombre ; nb++)
 		{
 			_putchar(str[nb]);
 		}
-	 }
+	}
+	_putchar('\n');
 }
