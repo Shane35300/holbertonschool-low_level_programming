6251fda8b352825477037f49690f1c56f31a4754
shane <6874@holbertonstudents.com>
2023-06-28 09:26:58 +0200

    feel good

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -10,14 +10,14 @@ void rev_string(char *s)
 	int nb = 0;
 	char temp;
 
-	 while (s[nombre] != '\0')
-	 {
-		nombre++;
-	 }
-	 for (nb = 0 ; nb < (nombre / 2) ; nb++)
-	 {
+while (s[nombre] != '\0')
+{
+	nombre++;
+	}
+	for (nb = 0 ; nb < (nombre / 2) ; nb++)
+	{
 		temp = s[nb];
 		s[nb] = s[nombre - nb - 1];
 		s[nombre - nb - 1] = temp;
-	 }
+		}
 }
