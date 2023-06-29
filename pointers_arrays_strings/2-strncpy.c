a495238e0f70f508dab068f4502f0cb3fd27996e
shane <6874@holbertonstudents.com>
2023-06-29 15:53:17 +0200

    jijij

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _strncpy - strncpy
- * Description: work exactly like strncpy
- *@dest: dest
- *@src: src
- *@n: n
- * Return: return
+* _strncpy - strncpy
+* Description: work exactly like strncpy
+*@dest: dest
+*@src: src
+*@n: n
+* Return: return
 */
 char *_strncpy(char *dest, char *src, int n)
 {
