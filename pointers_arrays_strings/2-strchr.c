3e64c6b40d9c80d25898bb44bf967d6735151220
shane <6874@holbertonstudents.com>
2023-06-30 12:38:56 +0200

    ko

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _strchr - fonction
+* Description : locate a caracter in a string
+* @s: string
+* @c: caracter
+* Return: return
+*/
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] == '\0' || s[i] == 'c')
+	{
+		i++;
+	}
+	return (s);
+}
