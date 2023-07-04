4553c9cf164354a94eebbb5937ad6e69ba95caf2
shane <6874@holbertonstudents.com>
2023-07-04 14:41:52 +0200

    good

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,20 +1,36 @@
+
+/**
+ * prime - fonction name
+ * Description: print a string
+ * @n: power
+ * @i: jij
+ * Return: return lenght
+*/
+int prime(int i, int n)
+{
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	if (i == (n - 1))
+	{
+		return (1);
+	}
+	return (prime(i + 1, n));
+}
+
 #include "main.h"
 /**
- * _sqrt_recursion - fonction name
+ * is_prime_number - fonction name
  * Description: print a string
  * @n: power
  * Return: return lenght
 */
-int _sqrt_recursion(int n, int i)
+int is_prime_number(int n)
 {
-	if (n == i * i && i % 2 == 0)
+	if (n < 2)
 	{
-		return (i);
+		return (0);
 	}
-	else if (i == 0)
-	{
-		return (-1);
-	}
-	else
-	return (n * _pow_recursion(n, i - 1));
+	return (prime(2, n));
 }
