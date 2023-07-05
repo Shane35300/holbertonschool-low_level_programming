228cf28abddf412d410c3fd3820aaaa89a48b7a3
shane <6874@holbertonstudents.com>
2023-07-05 16:38:12 +0200

    2 args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - function name
+ * Description: program that prints its name
+ * @argc: arg nbre
+ * @argv: arg array
+ * Return: return
+*/
+int main(int argc, char *argv[])
+{
+	(void)(argv);
+
+	if (argc > 0)
+	{
+		printf("%d\n", argc -1);
+	}
+	return (0);
+}
