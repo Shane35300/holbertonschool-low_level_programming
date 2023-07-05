5340e61a16b344a9a33f7da201b15ba2ed81ef88
shane <6874@holbertonstudents.com>
2023-07-05 15:55:42 +0200

    ok

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - function name
+ * Description: program that prints its name
+ * Return: return
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
