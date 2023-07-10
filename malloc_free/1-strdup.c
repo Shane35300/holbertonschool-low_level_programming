e0bb1543c0f33091da0b659213a29b88e0dcdae5
shane <6874@holbertonstudents.com>
2023-07-10 13:52:02 +0200

    good

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - function
+ * Description: creates an array of char
+ * @str: string
+ * Return: retourne
+*/
+char *_strdup(char *str)
+{
+	int nb = 0;
+	char *ptr = 0;
+	unsigned int i;
+
+	while ( str[nb] != '\0')
+	{
+		nb++;
+	}
+	ptr = (char *)malloc((nb + 1) * sizeof(char));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0 ; i < nb ; i++)
+	{
+		ptr[i] = str[i];
+	}
+	free(ptr);
+	return (ptr);
+}
