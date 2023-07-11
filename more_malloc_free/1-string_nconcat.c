287cf97dec3bf75cc43dcf856825e6e4e46bcb76
shane <6874@holbertonstudents.com>
2023-07-11 14:03:43 +0200

    ko

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -23,15 +23,21 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	ptr = (char *)malloc(sizeof(char) * (strlen(s1) + n + 1));
 
 	if (ptr == NULL)
-	return ("");
+	return (NULL);
 
 	for (i = 0 ; i < strlen(s1) ; i++)
 	{
 		ptr[i] = s1[i];
+
+		if (ptr[i] == '\0')
+		return ("");
 	}
 	for (j = 0 ; j < n ; j++)
 	{
 		ptr[i + j] = s2[j];
+
+		if (ptr[i + j] == '\0')
+		return ("");
 	}
 	return (ptr);
 }
