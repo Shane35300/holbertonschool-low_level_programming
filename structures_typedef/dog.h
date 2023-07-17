ed3e13340bf73ae712f00f7ad599a5fd8f61ec22
shane <6874@holbertonstudents.com>
2023-07-17 10:25:29 +0200

    ok du 1er coup

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - short description
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description:  longer description
+*/
 struct dog
 {
 	char *name;
