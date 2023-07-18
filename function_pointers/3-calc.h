26cae73b202a01cc89ec14e71f3a98a0a20bc318
shane <6874@holbertonstudents.com>
2023-07-18 15:11:14 +0200

    vfr

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -11,6 +11,9 @@ typedef struct op
 char *op;
 int (*f)(int a, int b);
 } op_t;
+
+int main(int argc, char *argv[]);
+int (*get_op_func(char *s))(int, int);
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -12,6 +12,13 @@ int op_add(int a, int b)
 {
 	return (a + b);
 }
+/**
+ * op_sub - sub
+ * Description: sub
+ * @a: a
+ * @b: b
+ * Return: return
+*/
 int op_sub(int a, int b)
 {
 	if ((a - b) < 0)
@@ -19,14 +26,35 @@ int op_sub(int a, int b)
 
 	return (a - b);
 }
+/**
+ * op_mul - multiplication
+ * Description: mul
+ * @a: a
+ * @b: b
+ * Return: return
+*/
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+/**
+ * op_div - division
+ * Description: div
+ * @a: a
+ * @b: b
+ * Return: return
+*/
 int op_div(int a, int b)
 {
 	return (a / b);
 }
+/**
+ * op_mod - modulo
+ * Description: mod
+ * @a: a
+ * @b: b
+ * Return: return
+*/
 int op_mod(int a, int b)
 {
 	return (a % b);
