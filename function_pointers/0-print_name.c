f492b08f12b549d2dc5d8f20a825ad6dd69047c0
shane <6874@holbertonstudents.com>
2023-07-18 09:48:20 +0200

    lpo

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - print name
+ * Description: print a name in uppecase and lowercase
+ * @name: name
+ * @f: ptr to function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
