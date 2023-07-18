36963d5e90e55a4ab6a3f7e56e8546e9cf9c885f
shane <6874@holbertonstudents.com>
2023-07-18 12:23:37 +0200

    gt

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * int_index - executes a function given
+ * Description: print a name in uppecase and lowercase
+ * @array: pointer of array
+ * @size: taille de l'array
+ * @cmp: action
+ * Return: ret
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL)
+	return (-1);
+
+	if (size <= 0)
+	return (-1);
+
+	for (i = 0 ; i < size ; i++)
+	{
+		if ((*cmp)(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
