da9ee3a26c5b844700a6de7e65e20a3ec93887a8
shane <6874@holbertonstudents.com>
2023-08-04 13:19:12 +0200

    commit2 task4 p3

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -14,13 +14,14 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (n == NULL)
 	return (-1);
 
-	if (*n == 0)
-	return (-1);
-
 	if (index >= (sizeof(unsigned long int) * 8))
 	return (-1);
 
 	mask = 1UL << index;
+
+	if ((mask & *n) == 0)
+	return (1);
+
 	*n ^= mask;
 	return (1);
 }
