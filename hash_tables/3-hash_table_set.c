5c4f5b1ed626316d21fa78ae0ff71c259d40a0dd
shane <6874@holbertonstudents.com>
2023-08-10 12:35:59 +0200

    modif task3 p2

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -40,6 +40,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	else
 		{
+			while (current_node != NULL)
+			{
+				if (strcmp(key, current_node->key) == 0)
+				{
+					current_node->value = strdup(value);
+					return (1);
+				}
+				current_node = current_node->next;
+			}
+			current_node = ht->array[index];
 			new_node->next = current_node;
 			ht->array[index] = new_node;
 		}
